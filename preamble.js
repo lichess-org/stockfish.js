71589163bb8eb4e93fbd1e65a1e2520b250f3930
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-07-16 17:14:22 +0200

    stockfish.js: Add script to build release with preamble

---
+++ preamble.js
@@ -0,0 +1,15 @@
+/*!
+ * Stockfish copyright T. Romstad, M. Costalba, J. Kiiski, G. Linscott
+ * and other contributors.
+ *
+ * Multi-variant support by Daniel Dugovic:
+ * https://github.com/ddugovic/Stockfish
+ *
+ * Released under the GNU General Public License v3.
+ *
+ * Compiled to JavaScript by Niklas Fiekas <niklas.fiekas@backscattering.de>
+ * using Emscripten.
+ *
+ * https://github.com/niklasf/stockfish.js
+ */
+
