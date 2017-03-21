cb39febc8353caaa8ae981d4a74f7a1e7f0cb084
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-03-21 12:56:46 +0100

    stockfish.wasm: Let uglifyjs strip dead code

--- release.sh
+++ release.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -e
 cd src
+
 make clean
 make ARCH=js build -B -j4
 cat ../preamble.js stockfish.js > ../stockfish.js
+
 make clean
 make ARCH=wasm build -B -j4
-uglifyjs stockfish.js | sed "s/SF_VERSION/$(sha256sum stockfish.wasm | cut -c1-8)/" | cat ../preamble.js - > ../stockfish.wasm.js
+uglifyjs --compress --mangle -- stockfish.js | sed "s/SF_VERSION/$(sha256sum stockfish.wasm | cut -c1-8)/" | cat ../preamble.js - > ../stockfish.wasm.js
 cp stockfish.wasm ../stockfish.wasm
+
 cd ..
