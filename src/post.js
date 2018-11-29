1016e37e53e80d974bf828386a46394efca87145
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-11-29 19:55:09 +0100

    stockfish.js: Fix asmjs after emscripten v1.38.1

--- src/post.js
+++
@@ -1,4 +0,0 @@
-onmessage = function (e) {
-  if (e.data == 'quit') close();
-  else Module.ccall("uci_command", "number", ["string"], [e.data]);
-};
--- src/pre.js
+++ src/pre.js
@@ -1,5 +1,24 @@
-var Module = {
-  print: function(stdout) {
-    postMessage(stdout);
-  }
-};
+Module = (function () {
+  var queue = [];
+
+  onmessage = function (e) {
+    if (e.data == 'quit') close();
+    else if (queue !== null) queue.push(e.data);
+    else Module.ccall('uci_command', 'number', ['string'], [e.data]);
+  };
+
+  return {
+    locateFile: function(file) {
+      return file;
+    },
+    print: function(stdout) {
+      postMessage(stdout);
+    },
+    postRun: function() {
+      for (var i = 0; i < queue.length; i++) {
+        Module.ccall('uci_command', 'number', ['string'], [queue[i]]);
+      }
+      queue = null;
+    }
+  };
+})();
